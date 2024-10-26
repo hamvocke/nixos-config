50a0d9ee0df612b1de79ab608ef41ca38b51c11b
Ham Vocke <ham@hamvocke.com>
2024-10-26 15:57:20 +0200

    disable wifi power management

--- configuration.nix
+++ configuration.nix
@@ -15,9 +15,11 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "kronos"; # Define your hostname.
+
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.wifi.powersave = false;
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
